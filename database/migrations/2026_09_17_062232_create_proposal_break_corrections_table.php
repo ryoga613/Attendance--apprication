ded23387aedafa72dd261256273963025242bd0c
Ryoga <ryogatoratora814@gmail.com>
2026-09-18 23:58:19 +0900

    create_proposal_break_corrections_tableの修正を行いました

--- database/migrations/2026_09_17_062232_create_proposal_break_corrections_table.php
+++ database/migrations/2026_09_17_062232_create_proposal_break_corrections_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('proposal_break_corrections', function (Blueprint $table) {
             $table->id();
             $table->foreignId('attendance_correction_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('proposal_break_id')->constrained()->cascadeOnDelete()->nullable();
             $table->dateTime('break_start_at')->nullable();
             $table->dateTime('break_end_at')->nullable();
             $table->timestamps();
