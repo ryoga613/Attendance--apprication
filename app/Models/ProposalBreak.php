51f3bde3d806f0f015af9d4e291f5323b43187a9
Ryoga <ryogatoratora814@gmail.com>
2026-09-19 01:31:29 +0900

    ProposalBreakModel

--- app/Models/ProposalBreak.php
+++ app/Models/ProposalBreak.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProposalBreak extends Model
 {
@@ -17,14 +18,16 @@ class ProposalBreak extends Model
         'break_time',
     ];
 
-    public function user() : BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+   
 
     public function attendance() : BelongsTo
     {
         return $this->belongsTo(Attendance::class);
+    }
+
+    public function proposalBreakCorrections() : HasMany
+    {
+        return $this->hasMany(ProposalBreakCorrection::class);
     }
     
 }
