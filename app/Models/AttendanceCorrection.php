ec6a75bd5c6a4a99dee6349ff87569f531199bdc
Ryoga <ryogatoratora814@gmail.com>
2026-09-18 15:24:32 +0900

    AttendanceCorrectionModelの実装を行いました

---
+++ app/Models/AttendanceCorrection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class AttendanceCorrection extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'attendance_id',
+        'correction_type',
+        'correction_date',
+        'correction_start_time',
+        'correction_end_time',
+        'correction_break_time',
+        'correction_work_time',
+    ];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function attendance() : BelongsTo
+    {
+        return $this->belongsTo(Attendance::class);
+    }
+
+    public function proposalBreakCorrections() : HasMany
+    {
+        return $this->hasMany(ProposalBreakCorrection::class);
+    }
+}
