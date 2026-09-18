8417836364932384d50c7cfdbf3fc48e16e1865b
Ryoga <ryogatoratora814@gmail.com>
2026-09-18 14:11:12 +0900

    UserModelの実装を行いました

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Attendance;
+use App\Models\AttendanceCorrection;
+use App\Models\ProposalBreak;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class User extends Authenticatable
 {
@@ -21,6 +26,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'admin_status',
     ];
 
     /**
@@ -42,4 +48,28 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function attendances() : HasMany
+    {
+        return $this->hasMany(Attendance::class);
+    }
+
+    public function proposalBreak(): HasManyThrough
+    {
+        return $this->hasManyThrough(ProposalBreak::class, AttendanceCorrection::class);
+    }
+
+
+
+    public function attendanceCorrections() : HasMany
+    {
+        return $this->hasMany(AttendanceCorrection::class);
+    }
+
+    public function proposalBreakCorrections() : HasManyThrough
+    {
+        return $this->hasManyThrough(ProposalBreakCorrection::class, AttendanceCorrection::class);
+    }
+
 }
