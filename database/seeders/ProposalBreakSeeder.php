abb991d36d5e9695f6bdb93f5e77024bbeffc5d1
Ryoga <ryogatoratora814@gmail.com>
2026-09-20 13:01:43 +0900

    ProposalBreakSeederの実装を行いました

---
+++ database/seeders/ProposalBreakSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use \App\Models\Attendance;
+use \App\Models\ProposalBreak;
+
+class ProposalBreakSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $attendances = Attendance::all();
+        foreach ($attendances as $attendance) {
+            ProposalBreak::create([
+                'attendance_id' => $attendance->id,
+                'break_start_at' => $attendance->work_date->copy()->setTime(12, 0, 0),
+                'break_end_at' => $attendance->work_date->copy()->setTime(13, 0, 0),
+            ]);
+        }
+    }
+}
